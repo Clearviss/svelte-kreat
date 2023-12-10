ef5c4535fb3f1121bb29a9b420326fd6d5610794
Clearviss <domi500domi@gmail.com>
2023-12-10 20:41:35 +0100

    quik

--- src/lib/database.ts
+++
@@ -1,4 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-import prisma from '@prisma/client'
-
-export const db = new prisma.PrismaClient()
--- src/routes/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Kreat nowa edycja</h1>
